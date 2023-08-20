4d5af7fee212ebdb43efe562a959198fc9e81b1c
28jackiiee <66331105+28jackiiee@users.noreply.github.com>
2023-08-20 08:56:47 -0700

    did

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import Project from './Project.tsx'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
